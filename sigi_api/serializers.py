9801a91f10aa8f840ec9856b6b92ed7a2323061d
icanbe444 <icanbe444@gmail.com>
2023-02-09 21:07:27 +0100

    Fixed the tranref and confirm payment request body

--- sigi_api/serializers.py
+++ sigi_api/serializers.py
@@ -1,25 +1,15 @@
 from rest_framework import serializers
-import datetime
 from datetime import datetime
-
-
-
-# class SearchSerializer(serializers.Serializer):
-#    integration_code = serializers.RegexField(required=True, regex=r"[0-9A-Za-z]+")
-#    payment_number = serializers.RegexField(required = True, regex=r"[0-9A-Za-z]+")
-
+from phonenumber_field.serializerfields import PhoneNumberField
    
-   
-   
-   
+
 class PaymentSerializer(serializers.Serializer):
-   integration_code     = serializers.RegexField(required=False, regex=r"[0-9A-Za-z]+")
-   payer_number         = serializers.RegexField(required=False, regex=r"[0-9A-Za-z]+")
+   payer_number         = PhoneNumberField(allow_blank=False, required=True)
    payment_number       = serializers.RegexField(required=False, regex=r"[0-9A-Za-z]+")
    tranref              = serializers.RegexField(required=False, regex=r"[0-9A-Za-z]+")
-   payment_date         = serializers.DateTimeField(default=datetime.now)
    payment_amount       = serializers.DecimalField(max_digits=11, decimal_places=2)
    payment_description  = serializers.CharField()
+   
 
 
    
@@ -28,17 +18,15 @@ class PaymentSerializer(serializers.Serializer):
    
 class ValidationSerializer(serializers.Serializer):
    payment_number = serializers.RegexField(required = True, regex=r"[0-9A-Za-z]+")
-   integration_code = serializers.RegexField(required=True, regex=r"[0-9A-Za-z]+")
    
    def CheckValues(serializer):
       serializer.is_valid()
       payment_number = serializer.data.get("payment_number", False)
-      integration_code = serializer.data.get("integration_code", False)
+      
     
       if (payment_number == False):
          return "payment_number empty"
-      if (integration_code == False):
-         return "payment_number empty"
+      
       
       return True
     
--- sigi_api/views.py
+++ sigi_api/views.py
@@ -7,168 +7,164 @@ from .serializers import ValidationSerializer, PaymentSerializer
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 import os
+from rest_framework.exceptions import ValidationError
 import requests, json
+import sys
+from datetime import datetime
 
-base_url = os.getenv('base_url')
+
+
+base_url            = os.getenv('base_url')
+auth                = os.getenv('auth')
+cookie              = os.getenv('cookie')
+integration_code    = os.getenv('integration_code')
 
 headers = {
-            'Authorization': 'Basic ZGdpLXd5czpkZ2ktd3lzLWludGVncmF0aW9uLXYz',
+            'Authorization': f'Basic {auth}',
             'Content-Type': 'application/json',
-            'Cookie': 'JSESSIONID=ACAD8AA4116EBB2F297259DEF4D3F93C'
+            'Cookie': cookie
 }
 
-def CheckValues(serializer):
-    serializer.is_valid()
-    integration_code = serializer.data.get("integration_code", False)
-    payment_number = serializer.data.get("payment_number", False)
-    
-    if (integration_code == False):
-        return "integration_code empty"
-    if (payment_number == False):
-        return "payment_number empty"
-   
-    return True
-    
-def validation_req(self, data, request):
-        
-        try:
-            serializer = self.get_serializer(data=request.data)
-            ans = CheckValues(serializer)
-            
-            if (ans == "integration_code empty"):
-                return ({"message" :"integration_code empty is empty.", "status":"failed"}, False)
-            if (ans == "payment_number empty"):
-                return ({"message" :"tin_number empty is empty.", "status":"failed"}, False)
-            if not serializer.is_valid(raise_exception=True):
-                return ({"message" :" Please enter the correct details", "status":"failed"}, False)
-            print("Information is verified.")
-        except Exception as e:
-            print(e.args[0])
-            return ( e.args[0],  False)
-        data    = serializer.validated_data
-        path    = "/api/findPaymentByPaymentNumber"
-        url     = str(base_url) + str(path)
-        payload = {
-                "integration_code":data["integration_code"] ,
-                "payment_number":data["payment_number"],
-                }
-        try:
-            response = requests.request("POST", url, headers=headers, data=json.dumps(payload))
-            print("The url "+url+" went through.")
-            
-        except:
-            return ( "We cannot connect to the host ", False)
-            
-        try:
-            if response.status_code== 200:
-                res = response.json()
-                return (res, True)
-            else:
-                print(response.status_code)
-                return ( "Line 67", False)
-        except:
-            return ( "We cannot connect to the ", False)
-        
-       
-# class ValidationView(GenericAPIView):
-#     """
-#     To validate 
-#     """
-    
-#     # permission_classes = (IsAuthenticated, )
-#     serializer_class        = ValidationSerializer
-#     def post(self, request, *args, **kwargs):
-      
-#         data = request.data
-#         try:
-#             resp, status = validation_req(self, data, request)
-#         except:
-#             return Response({"Response":{"message":"Validation Failed.","status":"Failed"}})
-      
-#         if status:
-#             print("This is Response for Successful Validation: ", resp)
-#             return Response({"Response": {"Customer" :"Validation Successful","status":"Success"}})
-#         else:
-#             print("This is Response for failed Validation: ", resp)
-#             return ({"Response":{"message":"Failed transaction","status":"Failed"}})
 
+def CheckValues(serializer):
+    if not serializer.is_valid():
+        raise ValidationError(serializer.errors)
+    integration_code = serializer.data.get("integration_code")
+    payment_number = serializer.data.get("payment_number")
+    return (
+        (
+            "success", True
+            if payment_number else "Payment number cannot be empty", False
+        )
+        if integration_code
+        else ("Integration code cannot be empty", False)
+    )
+    
+
+
+
+
+class ValidationView(GenericAPIView):
+    """
+    To validate
+    """
+    # permission_classes = (IsAuthenticated, )
+    serializer_class = ValidationSerializer
+    url = f"{str(base_url)}/api/findPaymentByPaymentNumber"
+
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        status = CheckValues(serializer)
+
+        if not status:
+            return Response({"Response":{"message" :"", "status":"Failed"}}, status=status.HTTP_400_BAD_REQUEST)
+
+        print("Information is verified.")
+
+
+        data    = serializer.validated_data
+        payload = {
+                    "payment_number": data["payment_number"],
+                    "integration_code": integration_code
+                }
+
+        try:
+            response = requests.request("POST", self.url, headers=headers, data=json.dumps(payload))
+            print(f"The url {self.url} went through.")
+            if response.status_code != 200:
+                return Response({"Response":{"message": "An error occurred while connecting to ****","status":"Failed"}} , status=status.HTTP_400_BAD_REQUEST)
+            res = response.json()
+        except Exception:
+            return Response({"Response":{"message": "An error occurred while connecting to ****","status":"Failed"}} , status=status.HTTP_400_BAD_REQUEST)
+
+        # VAS requirement
+
+        print({
+            "vendor_url": self.url,
+            "res": str(res) or None,
+            "vas_payload": str(request.data),
+            "vas_url": str(request.path),
+
+        }, file=sys.stdout)
+        
+        return Response({"Response":{"message":res,"status":"Success"}})
+        
+        
 
 class PaymentView(GenericAPIView):
     # permission_classes = (IsAuthenticated, )
     
-    serializer_class        = PaymentSerializer
+    serializer_class = PaymentSerializer
+    url = f"{str(base_url)}/api/confirmPayment"
+
     def post(self, request, *args, **kwargs):
         # import pdb
         # pdb.set_trace()
         serializer = self.get_serializer(data=request.data)
-        ans = CheckValues(serializer)
-            
-        if not serializer.is_valid(raise_exception=True):
-            return Response({"Response":{"message" :" Please enter the correct details", "status":"Failed"}}, status=status.HTTP_400_BAD_REQUEST)
+        status = CheckValues(serializer)
+
+        if not status:
+            return Response({"Response":{"message" :"Pleasa enter correct details", "status":"Failed"}}, status=status.HTTP_400_BAD_REQUEST)
         print("Information is verified.")
-        
-        data    = serializer.validated_data
-        path = "/api/confirmPayment"
-        url = str(base_url) + str(path)
         payload = {
-                    "integration_code"      : request.data.get("integration_code"),
+                    "integration_code"      : integration_code,
                     "payer_number"          : request.data.get("payer_number"),
                     "payment_number"        : request.data.get("payment_number"),
-                    "payment_ext_reference" : request.data.get("tranref"),
-                    "payment_date"          : request.data.get("payment_date"),
+                    "payment_ext_reference" : request.data.get("tranref"), 
+                    "payment_date"          : datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
                     "payment_amount"        : request.data.get("payment_amount"),
-                    "payment_description"   : request.data.get("payment_description"),
+                    "payment_description"   : request.data.get("payment_description")
         }
-       
+
         try:
-            response = requests.request("POST", url, headers=headers,data=json.dumps(payload))
+            response = requests.request("POST", self.url, headers=headers, data=json.dumps(payload))
             print("Payment processing")
             res = response.json()
-            if res.get("status") == "P":
-                return Response({"Response":{"message": "Payment completed successfully", "status":"Success"}})
-            else:
-                return Response({"Response":{"message": "Line 128","status":"Failed"}} , status=status.HTTP_400_BAD_REQUEST)
+            try:
+                if res["status"] != "P":
+                    print(res)
+                    return Response({"Response":{"message": res["status"],"status":"Failed"}} )
+            except Exception as e:
+                print(str(e.args[0]))
+                return Response({"Response":{"message": "Status not available. Check host connection","status":"Failed"}} )
         except Exception as e:
             print(e.args[0])
-            return  Response({"Response":{"message":"We cannot connect to the ", "status":"Failed"}})
+            return  Response({"Response":{"message":"Unable to make payment for now ", "status":"Failed"}})
 
-class ValidationView(GenericAPIView):
-    """
-    To search for payment use payment_number in the request body.
-    
-    """
-    # permission_classes = (IsAuthenticated, )
-    serializer_class        = ValidationSerializer
-    def post(self, request, *args, **kwargs):
+        print({
+            "vendor_url": self.url,
+            "res": str(res) or None,
+            "vas_payload": str(request.data),
+            "vas_url": str(request.path),
+
+        }, file=sys.stdout)
         
-        serializer = self.get_serializer(data=request.data)
-        ans = CheckValues(serializer)
-        if (ans == "payment_number empty"):
-            return Response({"Response":{"message" :"payment_number is empty.", "status":"Failed"}}, status=status.HTTP_400_BAD_REQUEST)
-            
-        if not serializer.is_valid(raise_exception=True):
-            return Response({"Response":{"message" :" Please enter the correct details", "status":"Failed"}}, status=status.HTTP_400_BAD_REQUEST)
-        print("Information is verified.")
-        
-        data    = serializer.validated_data
-        path    = "/api/findPaymentByPaymentNumber"
-        url     = str(base_url) + str(path)
-        payload = {
-                    "payment_number": data["payment_number"],
-                    "integration_code": data["integration_code"]
-            
-                }
-        
-        try:
-            response = requests.request("POST", url, headers=headers, data=json.dumps(payload))
-            print("The url "+url+" went through.")
-            if response.status_code== 200:
-                res = response.json()
-                return Response({"Response":{"message":res,"status":"Success"}})
-            else:
-                return Response({"Response":{"message": "Line 178","status":"Failed"}} , status=status.HTTP_400_BAD_REQUEST)
-        except:
-            return Response({"Response":{"message": "Line 180","status":"Failed"}} , status=status.HTTP_400_BAD_REQUEST)
-        
-        
-        
+        return Response({"Response":{"message": "Payment completed successfully", "status":"Success"}})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
